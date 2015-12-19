6105acc13d5aa9e7a1da75a6db8a910bfaf3e80b
davdkm <davd.km@gmail.com>
2015-12-18 21:52:26 -0800

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+	if speed < 60 && speed > 40
+		false
+	else
+		true
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
-	
-
-
