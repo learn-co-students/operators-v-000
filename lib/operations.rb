933e2b6e1ad6bc2dd391f146eeab837d7608a6b2
simque <simon.duncanson@gmail.com>
2016-01-15 08:38:22 -0500

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    true
+  else
+    false
+  end
 end
 
-
+# can refactor #unsafe? to simply, speed < 40 || speed > 60
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
