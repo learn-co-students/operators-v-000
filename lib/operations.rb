735f548d75df9ff97bb48ad0c5fd5ce89aea4825
reifnotreef <bridgerrhammond@gmail.com>
2016-01-21 11:25:01 -0600

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-
+	return true if speed > 60
+	return true if speed < 40
+	return false if speed < 60 && speed > 40
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
-	
-
-
