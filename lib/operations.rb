1c03f52d4d38f0511f756c2eaa4558adfe8de5e6
rafeeq-123 <rafeeq-123@hotmail.com>
2016-01-22 10:09:29 -0800

    Ternary

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+    return true if speed > 60
+    return true if speed < 40
+    return false if speed == 50
 end
-
 
 
 def not_safe?(speed)
-	
+
+  return true if speed > 60
+  return true if speed < 40
+  return false if speed == 50
+  20 > 10 ? true : false
 end
 	
-
-
