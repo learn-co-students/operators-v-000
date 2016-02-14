4f1c5875f13d3c844e57ee6b311b6772c66113ad
Lady Codington <myemailforcoding@gmail.com>
2016-02-14 11:45:36 -0500

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+	case
+	when speed > 60
+		true
+	when speed < 40
+		true
+	else
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	((speed < 40) || (speed > 60)) ? true : false
 end
-	
-
-
