8e4139162e29803550f350c7a2647beecec5c99b
ludaires <ludaires@gmail.com>
2016-04-01 22:18:38 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
-def unsafe?(speed)
+# age < 2 ? "baby" : "not a baby"
 
+
+def unsafe?(speed)
+ if speed < 40 || speed > 60
+  true
+ else speed.between?(40,60)
+  false
+ end
 end
 
 
 
 def not_safe?(speed)
-	
+  speed.between?(40,60) ? false : true
 end
-	
+
 
 
