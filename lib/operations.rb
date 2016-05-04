00c5b65d1934f4a5b055b3e7e9fe4a137c8ddd5d
rnhutsko <rnhutsko@gmail.com>
2016-05-04 11:24:55 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
 def unsafe?(speed)
 
+if speed < 40 or speed >60
+  unsafe = true
+else  
+  unsafe =false
+end
+
+unsafe
 end
 
 
 
 def not_safe?(speed)
-	
+	(speed < 40 or speed > 60) ? driver_is_safe = true : driver_is_safe = false
+driver_is_safe
 end
 	
 
