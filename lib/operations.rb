3739d6c3fbc918750cfccfd99730ebdde49fb08a
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-07-22 00:32:58 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+	  if speed < 40
+	   return true
+	 end
+	  if speed > 60
+	   return true
+	 end
+false
 end
-
+unsafe?(90)
 
 
 def not_safe?(speed)
-	
+		speed < 40  || speed > 60 ? true : false
 end
-	
-
-
