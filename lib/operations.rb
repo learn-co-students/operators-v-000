4cf5cb953195ffa197d6f1fbe92b6e02bbde16a5
alispace <alispace410@gmail.com>
2016-09-15 08:30:48 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
-require 'pry'
-
 def unsafe?(speed)
   if speed > 60 || speed < 40
     true
-  elsif speed > 40 || speed < 60
+  else
     false
   end
 end
 
+
+
 def not_safe?(speed)
-  speed >60 || speed < 40 ? true: false
+  speed > 60 || speed < 40? true : false
 end
