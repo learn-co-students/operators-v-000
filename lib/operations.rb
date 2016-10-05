00a908089edbaab060ad1077eb59d68c34878196
Alexis Roman <alexismmroman21@gmail.com>
2016-10-05 21:13:19 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
+	if 40 > speed || speed > 60
+		return true
+	else
+		false
+
+	end
 
 end
 
 
 
 def not_safe?(speed)
-	
+
+40 > speed || speed > 60 ? true : false
+
 end
-	
-
-
