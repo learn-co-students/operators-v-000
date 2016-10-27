9b417a1cc49e84c697c5d804c8838e68d39b0d19
Keegan Leitz <kjleitz@gmail.com>
2016-10-27 21:09:56 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+	if speed.between?(40, 60)
+		false
+	else
+		true
+	end
 end
-
-
 
 def not_safe?(speed)
-	
+	speed.between?(40, 60) ? false : true
 end
-	
 
-
+# No need for all that jazz! Could just do this:
+#
+# def dangerous?(speed)
+# 	!speed.between?(40, 60)
+# end
