8d18de43e94fa3d87477755da491eb41beeb4a21
HANA LEANDER <hana0070@gmail.com>
2016-11-14 00:50:01 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+	#conditional ? action_if_true : action_if_false
+	if speed > 60 || speed < 40
+		return true
+	else
+		return false
+	end
 end
-
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
-	
-
-
