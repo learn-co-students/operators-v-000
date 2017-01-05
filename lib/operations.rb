6752c1c4f2f05a3fc6f92074675ec474d1a13e01
Catherine Cordova <ccordova41@gmail.com>
2017-01-05 16:55:27 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,28 @@
 def unsafe?(speed)
+ if speed < 40
+		 return true
 
+	 elsif speed >60
+ 		return true
+
+	else speed
+		return false
+
+	end
 end
-
-
 
 def not_safe?(speed)
-	
+
+speed ? "true" : "false"
+
+    if speed < 40
+        return true
+
+    elsif speed >60
+       return true
+       
+     else speed
+       return false
+
+  end
 end
-	
-
-
