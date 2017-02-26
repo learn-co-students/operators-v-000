649ef462402780fc21f46dd354cf074595a9d7fe
Vipul Sharma <azvipul.sharma@gmail.com>
2017-02-26 21:00:20 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+	if speed.between?(40, 60)
+		false
+	else
+		true
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed.between?(40, 60)
+		false
+	else
+		true
+	end
 end
-	
-
-
