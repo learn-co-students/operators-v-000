54890a9796f8e9e5acb7b57d954f5ad8b33900ae
Daniela Carvajal <d.carvajal.agama@gmail.com>
2017-03-03 21:46:49 +0000

    speed done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+ if speed <= 40 || speed >= 60
+	 true
+ else speed == 50
+	 false
+ end
 end
-
 
 
 def not_safe?(speed)
-	
+  (speed <= 40 && speed >= 60) || speed == 50 ? false : true
 end
-	
-
-
