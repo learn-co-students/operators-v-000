07ce2f91003a7d3882269f5fd9806bb8ddbde2f5
Leonardo Guzman <lguzman@me.com>
2017-03-22 04:18:16 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
-def unsafe?(speed)
 
+
+def unsafe?(speed)
+  # '&&' will not work
+  #needs to be "||" becuase the speed passed wont be both lower than 40 and above 60
+  if (speed < 40) || (speed > 60)
+    return true
+  elsif speed.between?(40, 60)
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  (speed < 40) || (speed > 60) ? true : false
 end
-	
-
-
