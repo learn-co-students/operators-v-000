1f2fd887b9168ba49708f4fa6cc86439094892ee
Derrick Chu <dc79627n@pace.edu>
2017-04-12 22:08:01 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+	if speed < 40 || speed > 60
+		return true
+	else
+		return false
+	end
 end
 
-
-
+#conditional ? action_if_true : action_if_false
 def not_safe?(speed)
-	
+	(speed < 40 || speed > 60) ? true : false
 end
-	
-
-
