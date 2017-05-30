7d1d29cb2bf6b48bc3563cb6942efd36079771d2
Susan Fowler <infopro94@gmail.com>
2017-05-30 23:08:29 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+	if speed >= 40 && speed <= 60
+		false
+		else speed > 60 || speed < 40
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+  speed >= 40 && speed <= 60 ? false : speed > 60 || speed < 40
 end
-	
-
-
