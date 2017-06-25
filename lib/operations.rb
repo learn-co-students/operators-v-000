fa0b5f77ded4e0479958d664f7233967db2cde0d
Mars Howard <marsbhoward@gmail.com>
2017-06-25 14:18:40 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,9 @@
 def unsafe?(speed)
-
+speed < 40 || speed > 60 ? (return true) : (return false)
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? (return true) : (return false)
 end
-	
-
-
