a0c77e5ac32ee47ec59ea956517c049a7f7c59f6
Jeremy <emata@rapidratings.com>
2017-06-27 11:27:02 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,8 @@
+
 def unsafe?(speed)
-  if speed < 40 || speed > 60
-    true
-  else
-    false
-  end
+  speed >= 40 && speed <= 60 ? false : true
 end
-
-
 
 def not_safe?(speed)
-	speed <= 60 && speed >= 40 ? false : true
+  speed >= 40 && speed <= 60 ? false : true 
 end
-	
-
-
