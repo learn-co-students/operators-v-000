ed64d5b90bd4a525df0274df1a577fd6a7984aa7
Yehuda Makarov <yehudamakarov@gmail.com>
2017-07-13 21:57:59 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
-def unsafe?(speed)
 
+
+def unsafe?(speed)
+	if speed > 60 
+		speed > 60
+	else
+		speed < 40
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 ? speed > 60 : speed < 40
 end
 	
 
