a2402420f090801fed199ddc29c76a2afdac3da1
rjcolmed <rjcolmed@gmail.com>
2017-09-12 18:05:33 +0000

    add #unsafe? and #not_safe?

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
+# defined per lab requirements
 def unsafe?(speed)
-
+	if speed < 40 || speed > 60
+		return true
+	else
+		return false
+	end
 end
 
-
+# # refactor of #unsafe?
+# def unsafe?(speed)
+# 	speed < 40 || speed > 60
+# end
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
