b3b11a2c227e51e6ab54178638fa39020cfabd36
Logan McAnsh <logan@mcan.sh>
2017-10-09 17:21:19 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+  safe = speed > 60 || speed < 40 ? true : false
+  return safe
 end
 
 
 
 def not_safe?(speed)
-	
+  safe = speed > 60 || speed < 40 ? true : false
+  return safe
 end
-	
+
 
 
