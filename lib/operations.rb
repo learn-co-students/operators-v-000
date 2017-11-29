1817edad45d566998746e4af3d6adc246b67677c
Eric Lin <esotericlin@gmail.com>
2017-11-29 01:23:49 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+	if speed > 60
+		true
+	elsif speed < 40
+		true
+	elsif speed > 40 || speed < 60
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed = speed > 60 || speed < 40 ? true: false
 end
-	
-
-
