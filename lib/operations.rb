fba2cd81ee53815c19ae576577c598129b0b8748
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-15 16:42:50 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,6 +7,7 @@ def unsafe?(speed)
 end
 
 #dont forget if, else statements need an 'end' and so does the method
+#don't need explicit 'return'
 
 
 
@@ -15,3 +16,16 @@ speed > 60 || speed < 40 ? true : false
 end
 
 #ternary operator does not need end
+
+#formal solution below
+#def unsafe?(speed)
+#if speed > 60
+# => true
+# elsif speed < 40
+# => false
+#end
+#end
+
+#def not_safe(speed)
+#speed < 40 || speed > 60 ? true : false
+#end
