40a59c51baa8d1c902e15375a943034aa9e2d771
Jordan Harris <jharris@wework.com>
2018-06-21 20:38:15 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
-def unsafe?(speed)
-
+def unsafe?(speed)  
+  if 60 < speed || speed < 40
+    return true
+  else false
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+	if 60 < speed || speed < 40
+    return true
+  else false
+  end
 end
-	
-
-
