ddba3c0f04af1f2f7b323b2f536b33c3f007d4f2
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-15 03:22:46 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
-def unsafe?(speed)
+speed = " ".to_i 
 
+def unsafe?(speed)
+  if !speed.between?(40, 60)
+    return true
+  else speed.between?(40, 60)
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	!speed.between?(40, 60) ? true : false
 end
 	
 
