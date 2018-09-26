dd9ce994874be7504a68e4ff499bbeaa72d996fb
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-26 01:18:48 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
-def unsafe?(speed)
 
+def unsafe?(speed)
+	if speed < 60 && speed > 40
+		false
+	else
+		true
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 60 && speed > 40 ? false : true
 end
-	
-
-
