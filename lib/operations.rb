8e21687e54095906a78224cdf1d8e22cfcc2812b
ktochow1 <ktochow1@binghamton.edu>
2018-09-27 18:37:29 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
-def unsafe?(speed)
 
+
+def unsafe?(speed)
+  if speed > 60 || speed < 40 
+    return true
+  else speed >= 40 && speed <= 60 
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  unsafe?(speed) ? true : false
+# 	speed < 40 ? true : false 
+# 	speed > 60 ? true : false 
+	#unless speed.between?(41, 59)
 end
 	
 
